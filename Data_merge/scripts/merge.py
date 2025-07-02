840037206a1ce8f49b68cccd052903d39bd7966e
Kef <hei.kephas@gmail.com>
2025-07-02 17:49:41 +0300

    feat: merge datas

---
+++ Data_merge/scripts/merge.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+df1 = pd.read_csv("C:/Users/kefuz/OneDrive/Desktop/Wether_data/ETL/data/processed/meteo_global.csv")
+df2 = pd.read_csv("C:/Users/kefuz/OneDrive/Desktop/Wether_data/Historical_data/data/processed/meteo_global.csv")
+
+columns = list(set(df1.columns).intersection(df2.columns))
+
+df1_common = df1[columns]
+df2_common = df2[columns]
+
+df_concat = pd.concat([df1_common, df2_common], ignore_index=True)
+
+df_concat.to_csv("C:/Users/kefuz/OneDrive/Desktop/Wether_data/Data_merge/data/processed/meteo_global.csv", index=False)
